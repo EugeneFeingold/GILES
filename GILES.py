32c649f112b0b2cb5bae8ebf156304e968e00f84
Mike Juarez <mikejuarez@gmail.com>
2014-06-26 17:27:32 -0700

    Adding in native Linux Joystick support

--- GILES.py
+++ GILES.py
@@ -1,89 +1,57 @@
 #!/usr/bin/python
 
-
-
-import cwiid
 import time
 import serial
+import pygame
+import RPi.GPIO as GPIO
+from pygame.locals import *
 
+GPIO.cleanup()
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
+GPIO.output(18, GPIO.LOW)
+
+pygame.init()
+j = pygame.joystick.Joystick(0)
+j.init()
 
 port = serial.Serial("/dev/ttyAMA0", baudrate=38400, timeout=3.0)
 port.write(chr(64))
 port.write(chr(192))
 
-
-#connecting to the Wiimote. This allows several attempts 
-# as first few often fail. 
-print 'Press 1+2 on your Wiimote now...' 
-wm = None 
-i=2 
-while not wm: 
-  try: 
-    wm=cwiid.Wiimote() 
-  except RuntimeError: 
-    if (i>10): 
-      quit() 
-      break 
-    print "Error opening wiimote connection" 
-    print "attempt " + str(i) 
-    i +=1 
-
-print "CONNECTED!"
-
-wm.rpt_mode = cwiid.RPT_BTN
-wm.led = 1
-
 prevm1 = 0
 prevm2 = 0
 
+GPIO.output(18, GPIO.HIGH)
 
 while True:
 	try:
-		m1 = 0
-		m2 = 0
-		mult = 1
-	
-		buttons = wm.state['buttons']
-		if (buttons & cwiid.BTN_B):
-			print "B"
-			mult = 2
-		
-		if (buttons & cwiid.BTN_1):
-			print "1"
-			m1 = m1 + 31
-			m2 = m2 + 31 
-		
-		if (buttons & cwiid.BTN_2):
-			print "2"
-			m1 = m1 - 31
-			m2 = m2 - 31
-		
-		if (buttons & cwiid.BTN_UP):
-			print "up"
-			m1 = m1 - 16
-			m2 = m2 + 16
-		
-		if (buttons & cwiid.BTN_DOWN):
-			print "down"
-			m1 = m1 + 16
-			m2 = m2 - 16
-		
-		m1 = min(m1, 63)
-		m1 = max(m1, -63)
-		m2 = min(m2, 63)
-		m2 = max(m2, -63)
-		
-		m1 = m1 * mult
-		m2 = m2 * mult
-	
-		m1 = prevm1 + (prevm1 - m1) / 2
-		m2 = prevm2 + (prevm2 - m2) / 2
-	
-		port.write(chr(64 + m1))
-		port.write(chr(192 + m2))
+		for event in pygame.event.get():
+			try:
+				if event.type == QUIT:
+					port.write(chr(64))
+					port.write(chr(192))
+					pygame.quit()
+
+				maxspeed = 18.0
+
+				m1 = int(j.get_axis(0)*maxspeed)
+				m2 = int(j.get_axis(1)*maxspeed)
+				
+				print "COORD: %s %s" % (str(m1), (m2))
+
+				port.write(chr(64 + m1))
+				port.write(chr(192 + m2))
+			except:
+				print "exception!!!"
+				GPIO.output(18, GPIO.LOW)
+				port.write(chr(64))
+				port.write(chr(192))
+				exit()
 	except:
+		print "double exception!!!"
 		port.write(chr(64))
 		port.write(chr(192))
-  
-  
-  
+		GPIO.output(18, GPIO.LOW)
+		exit()
